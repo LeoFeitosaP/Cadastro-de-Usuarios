f4a72f9758a6267ff35a0b9a51800419a072d072
LeoFeitosaP <leonardoperes342007@gmail.com>
2025-10-05 16:20:58 -0300

    Project Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Primeiro CRUD em Java
+
+## Descrição
+Este é um projeto simples de um CRUD (Create, Read, Update, Delete) em Java - Projeto feito com um protótipo para o cadastro de usuarios.
---
+++ src/main/java/com/leonardo/cadastro_usuario/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package com.leonardo.cadastro_usuario.service;
+
+import com.leonardo.cadastro_usuario.infrastructure.entity.Usuario;
+import com.leonardo.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void salvatUsuario(Usuario usuario) {
+        usuarioRepository.saveAndFlush(usuario);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado!")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email) {
+        usuarioRepository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorEmail(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado!"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getEmail())
+                .id(usuarioEntity.getId())
+                .build();
+
+        usuarioRepository.saveAndFlush(usuarioAtualizado);
+
+
+    }
+
+}
